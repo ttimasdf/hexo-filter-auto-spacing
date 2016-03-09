9bc5123a760654bec34ee60243844271efa83986
ttimasdf <ttimasdf@gmail.com>
2016-03-09 20:22:05 +0800

    Skip processing in tags

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var pangunode = require('pangunode');
 
-hexo.extend.filter.register('after_post_render', function(data) {
+hexo.extend.filter.register('before_post_render', function(data) {
   data.title = pangunode(data.title);
   data.content = pangunode(data.content);
-});
-
+  data.content = data.content.replace(/({%[^%{}]*?"[^%{}]*"[^%{}]*?%})/g,
+                                      function(m){
+                                        return m.replace(/({%[^%{}]*?")([^%{}]*?)("[^%{}]*?%})/,
+                                                         function(match, b, s, a){
+                                                           return b + s.replace(/[\s]/g, '')+a;
+                                                         }
+                                                        );
+                                      }
+                                     );
+})
